6fac0f43475715ec7506d21e9fdeda3d13ffae46
Priyabrata Singh <priyabratasingh2005@gmail.com>
2026-02-01 16:21:24 +0530

    add navigation items to header and create constants for nav links

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import NavItems from './NavItems'
 
 const Header = () => {
   return (
@@ -9,6 +10,11 @@ const Header = () => {
             <Link href='/'>
                 <Image src='/logo.png' alt='Lgo' width={140} height={32} className="h-8 w-auto cursor-pointer" />
             </Link>
+            <nav className='hidden sm:block'>
+                { /* navitems can be added here in future */ }
+                <NavItems />
+            </nav>
+        {/*userdropdown can be added here in future*/}   
         </div>
     </header>
   )
---
+++ lib/constants.ts
@@ -0,0 +1,5 @@
+const NAV_ITMES = [
+    { label: "Home", href: "/" },
+    { label: "About", href: "/about" },
+    { label: "Contact", href: "/contact" },
+    ]
