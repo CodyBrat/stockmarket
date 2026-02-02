fa9c82a4a3f3fa49c218c095fe350ec67791e1c9
Priyabrata Singh <priyabratasingh2005@gmail.com>
2026-02-02 23:11:47 +0530

    feat: refactor TradingViewWidget for improved layout and dynamic script URL handling

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,15 +2,25 @@ import TradingViewWidget from "@/components/TradingViewWidget"
 import { MARKET_DATA_WIDGET_CONFIG } from "@/lib/constants"
 
 const Home = () => {
+  const scriptUrl=`https://s3.tradingview.com/external-embedding/embed-widget-`;
   return (
     <div className="flex min-h-screen home-wrapper">
       <section className="grid w-full gap-8 home-section">
         <div className="md:col-span-1 xl:col-span-1">
           <TradingViewWidget
            title="Market Overview" 
-           scriptUrl="https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js"
+           scriptUrl={`${scriptUrl}market-overview.js`}
            config={MARKET_DATA_WIDGET_CONFIG} 
-           className="custom-chart"/>
+           className="custom-chart"
+           height={600}/>
+        </div>
+        <div className="md-col-span xl:col-span-2">
+          <TradingViewWidget
+           title="Stock Heatmap" 
+           scriptUrl={`${scriptUrl}stock-heatmap.js`}
+           config={MARKET_DATA_WIDGET_CONFIG} 
+           className="custom-chart"
+           height={600}/>
         </div>
       </section>
     </div>
