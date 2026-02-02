876484e3c20b18c46c5cb040a9d755fb4dc525e0
Priyabrata Singh <priyabratasingh2005@gmail.com>
2026-02-02 18:48:01 +0530

    feat: implement UserDropdown component with avatar and sign-out functionality

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -1,3 +1,4 @@
+'use client'
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -8,14 +9,31 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { Button } from "./ui/button"
+import { useRouter } from "next/navigation"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 const UserDropdown = () => {
+    const router=useRouter();
+    const handleSignOut= async()=>{
+        router.push('/sign-in');
+    }
+    const user ={ name: "John Doe", email:'contact@priyabrata.com' };
   return (
         <DropdownMenu>
         <DropdownMenuTrigger asChild>
-            <Button variant="outline">Open</Button>
+            <Button variant="ghost" className="flex items-center gap-3 text-gray-4 hover:text-yellow-500">
+                <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
+                    {user.name.split(' ').map(n => n[0]).join('')}
+                </AvatarFallback>
+                </Avatar>
+                <div className="hidden md-flex flex-col items-start">
+                    <span className="text-base font-medium text-gray-400">{user.name}</span>
+                </div>
+            </Button>
         </DropdownMenuTrigger>
-        <DropdownMenuContent>
+        <DropdownMenuContent className="">
             <DropdownMenuGroup>
             <DropdownMenuLabel>My Account</DropdownMenuLabel>
             <DropdownMenuItem>Profile</DropdownMenuItem>
