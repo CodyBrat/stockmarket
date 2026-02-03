12a9134b55ef19872a6c183fb7995afb2222d8cb
Priyabrata Singh <priyabratasingh2005@gmail.com>
2026-02-03 18:57:12 +0530

    feat: add InputField component and integrate it into SignUp form

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,5 +1,7 @@
 'use client'
+import InputField from '@/components/forms/inputField'
 import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 import React from 'react'
 import { useForm } from 'react-hook-form'
 
@@ -31,6 +33,31 @@ const SignUp = () => {
     <>
       <h1 className='form-title'>Sign Up & Personalize</h1>
       <form onSubmit={handleSubmit(onSubmit)} className='space-y-6'>
+        <InputField
+          name='fullName'
+          label='Full Name'
+          placeholder='John Doe'
+          register={register}
+          error={errors.fullName}
+          validation={{ required: 'Full Name is required', minLength:2}}
+        />
+        <InputField
+          name='email'
+          label='Email'
+          placeholder='johndoe@gmail.com'
+          register={register}
+          error={errors.email}
+          validation={{ required: 'Email is required', pattern: { value: /^\S+@\S+$/i, message: 'Invalid email address' } }}
+        />
+        <InputField
+          name='password'
+          label='Password'
+          placeholder='Enter your password'
+          type='password'
+          register={register}
+          error={errors.password}
+          validation={{ required: 'Password is required', minLength:8}}
+        />
         {/*Inputs}*/}
         <Button type='submit' className=' yellow-btn w-full mt-5' disabled={isSubmitting}>
           {isSubmitting ? 'Creating Account' : 'Start Your Investing Journey'}
